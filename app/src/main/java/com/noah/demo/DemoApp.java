6750dcc9ea387fb90b0cba26fc1ba0af1be1734a
libingcheng <libingcheng@changdou.vip>
2024-06-25 10:55:57 +0800

    新增utils和log

--- app/src/main/java/com/noah/demo/DemoApp.java
+++ app/src/main/java/com/noah/demo/DemoApp.java
@@ -5,15 +5,6 @@ import android.content.Context;
 import androidx.multidex.MultiDex;
 import androidx.multidex.MultiDexApplication;
 
-import com.cretin.www.cretinautoupdatelibrary.model.TypeConfig;
-import com.cretin.www.cretinautoupdatelibrary.model.UpdateConfig;
-import com.cretin.www.cretinautoupdatelibrary.utils.AppUpdateUtils;
-import com.cretin.www.cretinautoupdatelibrary.utils.SSLUtils;
-
-import java.util.concurrent.TimeUnit;
-
-import okhttp3.OkHttpClient;
-
 public class DemoApp extends MultiDexApplication {
     @Override
     protected void attachBaseContext(final Context base) {
@@ -24,8 +15,6 @@ public class DemoApp extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-
     }
-
 
 }
